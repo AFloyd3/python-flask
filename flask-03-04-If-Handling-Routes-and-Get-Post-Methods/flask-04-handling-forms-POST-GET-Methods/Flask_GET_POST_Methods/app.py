d5d1a39a2e0b545be0eba6164319d28b4309cb6d
AFloyd3 <aet3242018@gmail.com>
2023-01-12 14:31:11 -0700

    4flask

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
@@ -1,10 +1,9 @@
-# Import Flask modules
+
 from flask import Flask, render_template, request
-# Create an object named app
+
 app = Flask(__name__)
 
 
-# create a function named "lcm" which calculates a least common multiple values of two numbers. 
 def lcm(num1,num2):
     common_multiplications = [] 
     for i in range(max(num1, num2),num1*num2+1):
@@ -14,27 +13,25 @@ def lcm(num1,num2):
 
 
 
-# Create a function named `index` which uses template file named `index.html` 
-# send two numbers as template variable to the app.py and assign route of no path ('/') 
 @app.route("/")
 def index():
-    return render_template("index.html", methods=["GET"])
+    return render_template("index.html")
 
 
 
 
-# calculate sum of them using "lcm" function, then sent the result to the 
-# "result.hmtl" file and assign route of path ('/calc'). 
-# When the user comes directly "/calc" path, "Since this is a GET request, LCM has not been calculated" string returns to them with "result.html" file
-@app.route("/calc", methods=["GET", "POST"])
+
+@app.route("/calc", methods=["POST"])
 def calculate():
     if request.method == "POST":
+        print("Start of Request")
         num1 = request.form.get("number1")
         num2 = request.form.get("number2")
-        return render_template("result.html", result1 = num1, result2 = num2, lcm = lcm(int(num1),int(num2)), developer_name = 'Oktay')
+        lowest = lcm(int(num1), int(num2))
+        return render_template("result.html", result1 = num1, result2 = num2, lcm = lowest, developer_name = 'Amber')
     else:
-        return render_template("result.html", developer_name = "Oktay")
+        return "<h1> Error </h1>"
 
-# Add a statement to run the Flask application which can be debugged.
+
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(port=3000, debug=True)
